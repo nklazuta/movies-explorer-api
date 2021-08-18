d1d91dbce24672d419c5d4ea17cea3078f226c3c
nklazuta <nkgavshina@gmail.com>
2021-08-18 16:49:52 +0300

    fix: исправить настройки отправки куки

--- controllers/users.js
+++ controllers/users.js
@@ -93,7 +93,6 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          sameSite: true,
         })
         .send({ data: publicUser });
     })
