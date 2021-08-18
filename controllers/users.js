8f4dff0f3fefbbffc2cea17a69ec6fda78c6bc97
nklazuta <nkgavshina@gmail.com>
2021-08-18 17:59:23 +0300

    проверка токена

--- controllers/users.js
+++ controllers/users.js
@@ -93,8 +93,7 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          sameSite: false,
-          secure: true,
+          sameSite: true,
         })
         .send({ data: publicUser });
     })
